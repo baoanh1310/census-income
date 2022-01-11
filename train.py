d8153edbff4d9095f2d1f9970d3ca9dcca583a35
baoanh1310 <dataholicguy@gmail.com>
2022-01-11 20:09:22 +0700

    Train another drop model

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import os
 
-from config import BASIC_MODEL_PATH, TRAIN_DATA_PATH, TEST_DATA_PATH, DECISION_TREE_MODEL_PATH, KNN_MODEL_PATH
-from utils import read_data, prepare_data, train_logreg, evaluate, train_decision_tree, train_knn
+from config import BASIC_MODEL_PATH, TRAIN_DATA_PATH, TEST_DATA_PATH, DECISION_TREE_MODEL_PATH, KNN_MODEL_PATH, DROP_MODEL_PATH
+from utils import *
 
 # Import dataset
 train_df = read_data(TRAIN_DATA_PATH)
@@ -16,6 +16,13 @@ train_logreg(X_train, y_train, X_test, y_test, BASIC_MODEL_PATH)
 # Evaluation
 print("Evaluating for Logistic Regression Model...")
 evaluate(BASIC_MODEL_PATH, X_test, y_test)
+
+# Training drop model
+X_train, y_train, X_test, y_test = prepare_drop_data(train_df, test_df)
+train_logreg(X_train, y_train, X_test, y_test, DROP_MODEL_PATH)
+# Evaluation
+print("Evaluating for Drop Logistic Regression Model...")
+evaluate(DROP_MODEL_PATH, X_test, y_test)
 
 # Train decision tree
 # train_decision_tree(X_train, y_train, X_test, y_test, DECISION_TREE_MODEL_PATH)
