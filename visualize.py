198f1993628c25ec8b2a8d53d36c3b86c368d426
baoanh1310 <dataholicguy@gmail.com>
2022-01-11 21:16:56 +0700

    Ploting confuion matrix for each model

--- visualize.py
+++ visualize.py
@@ -6,8 +6,8 @@ from sklearn import preprocessing
 from sklearn.metrics import plot_confusion_matrix
 from joblib import load
 
-from utils import read_data, prepare_data
-from config import TRAIN_DATA_PATH, TEST_DATA_PATH, BASIC_MODEL_PATH
+from utils import *
+from config import *
 
 def visualize_corr(method='pearson'):
 	train_df = read_data(TRAIN_DATA_PATH)
@@ -31,14 +31,26 @@ def visualize_corr(method='pearson'):
 	img_name = "./plots/" + method + "_correlation.png"
 	plt.savefig(img_name)
 
-def visualize_confusion_matrix():
-	clf = load(BASIC_MODEL_PATH)
+def visualize_confusion_matrix(model_path):
+	clf = load(model_path)
 	train_df = read_data(TRAIN_DATA_PATH)
 	test_df = read_data(TEST_DATA_PATH)
-	X_train, y_train, X_test, y_test = prepare_data(train_df, test_df)
+	img_name = "./plots/confusion_matrix" + "_"
+
+	if model_path == BASIC_MODEL_PATH:
+		img_name = img_name + "basic.png"
+		X_train, y_train, X_test, y_test = prepare_data(train_df, test_df)
+	elif model_path == DROP_MODEL_PATH:
+		img_name = img_name + "drop.png"
+		X_train, y_train, X_test, y_test = prepare_drop_data(train_df, test_df)
+	elif model_path == OVERSAMPLING_MODEL_PATH:
+		img_name = img_name + "oversampling.png"
+		X_train, y_train, X_test, y_test = prepare_oversampling_data(train_df, test_df)
+
 	plot_confusion_matrix(clf, X_test, y_test, normalize='true')
-	plt.savefig("./plots/confusion_matrix.png")
+	plt.savefig(img_name)
 
 if __name__ == "__main__":
-	visualize_corr('spearman')
-	# visualize_confusion_matrix()
+	# visualize_corr('spearman')
+	visualize_confusion_matrix(DROP_MODEL_PATH)
+	visualize_confusion_matrix(OVERSAMPLING_MODEL_PATH)
